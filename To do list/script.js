0cede9e1e0d1e959f159c3a5a562322222df3734
Filipe William <lipe.will@outlook.com>
2020-09-05 18:30:40 -0300

    Quarto projeto pronto

--- To do list/script.js
+++ To do list/script.js
@@ -1,36 +1,47 @@
-const buttonAdd = document.querySelector('button');
-const inputPrincipal = document.querySelector('input');
-const divPrincipal = document.querySelector('.principal')
+let buttonAdd = document.querySelector('button');
+let inputPrincipal = document.querySelector('input');
+let divPrincipal = document.querySelector('.principal')
 
-//Create e add section
+//Create and add section
 const sectionCriada = document.createElement('section');
 sectionCriada.style.textAlign = 'center'
 sectionCriada.style.marginTop = '30px'
 document.body.appendChild(sectionCriada);
 
+//Main Function
+function adicionarAcao(event) {
 
-//Clone Button
-const buttonClone = document.createElement('button')
-buttonClone.innerText = 'REMOVE';
-buttonClone.style.backgroundColor = '#B02C2F';
-buttonClone.style.padding = '8px 13px 8px 12px'
+  //Clone Button
+  const buttonClone = document.createElement('button')
+  buttonClone.innerText = 'REMOVE';
+  buttonClone.style.backgroundColor = '#B02C2F';
+  buttonClone.style.padding = '8px 13px 8px 12px'
 
+  //Clone input
+  const inputClone = document.createElement('input')
+  inputClone.style = inputPrincipal.style;
+  inputClone.style.marginBottom = '8px'
 
-//Clone input
-const inputClone = document.createElement('input')
-inputClone.style = inputPrincipal.style;
-//Adicionar Clones
+  //Create a line break
+  const lineBreak = document.createElement('br')
 
-function adicionarAcao() {
-  document.getElementsByTagName('section')[0].appendChild(inputClone);
-  document.getElementsByTagName('section')[0].appendChild(buttonClone);
-  inputClone.innerText = inputPrincipal.innerText
+  //add Clones
+  sectionCriada.appendChild(inputClone)
+  sectionCriada.appendChild(buttonClone)
+  sectionCriada.appendChild(lineBreak)
+
+  //Reset input value
+  inputClone.value = inputPrincipal.value
+  inputPrincipal.value = '';
+
+  //Remove function
+  buttonClone.addEventListener('click', removerAcao);
+
+  function removerAcao() {
+    inputClone.remove();
+    buttonClone.remove();
+    lineBreak.remove();
+  }
 };
 
-function removerAcao() {
-  inputClone.remove();
-  buttonClone.remove();
-}
-
 buttonAdd.addEventListener('click', adicionarAcao);
-buttonClone.addEventListener('click', removerAcao)

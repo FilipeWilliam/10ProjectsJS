9c84b16bd17edf09694dd9c7aa666e4855069a47
Filipe William <lipe.will@outlook.com>
2020-09-23 15:19:06 -0300

    Começar a lógica novamente

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,53 +1,29 @@
 //Buttons
-const clearButton = document.getElementById('clear');
-const backspaceButton = document.getElementById('backspace');
-const divideButton = document.getElementById('divide');
-const multiplicationButton = document.getElementById('multiplication');
-const minusButton = document.getElementById('minus');
-const plusButton = document.getElementById('plus');
-const equalButton = document.getElementById('equal');
-const dotButton = document.getElementById('dot');
+const clearButton = document.getElementById("clear");
+const backspaceButton = document.getElementById("backspace");
+const divideButton = document.getElementById("divide");
+const multiplicationButton = document.getElementById("multiplication");
+const minusButton = document.getElementById("minus");
+const plusButton = document.getElementById("plus");
+const equalButton = document.getElementById("equal");
+const dotButton = document.getElementById("dot");
 
 //Result
-const resultSpace = document.querySelector('.result');
+const resultSpace = document.querySelector(".result");
 
 //Numbers
-const num0 = document.getElementById('num0');
-const num1 = document.getElementById('num1');
-const num2 = document.getElementById('num2');
-const num3 = document.getElementById('num3');
-const num4 = document.getElementById('num4');
-const num5 = document.getElementById('num5');
-const num6 = document.getElementById('num6');
-const num7 = document.getElementById('num7');
-const num8 = document.getElementById('num8');
-const num9 = document.getElementById('num9');
+const num0 = document.getElementById("num0");
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const num3 = document.getElementById("num3");
+const num4 = document.getElementById("num4");
+const num5 = document.getElementById("num5");
+const num6 = document.getElementById("num6");
+const num7 = document.getElementById("num7");
+const num8 = document.getElementById("num8");
+const num9 = document.getElementById("num9");
 
-//Read the button
-//PlusButton
-function plusOperation() {
-  let firstNumber = resultSpace.value;
-  console.log(firstNumber);
+//Function
+function printNumber(num) {
+  resultSpace.value = resultSpace.value + num;
 }
-plusButton.addEventListener('click', plusOperation)
-
-//EqualButton
-function equalOperation() {
-  let result = resultSpace.value + firstNumber;
-  resultSpace.value = result;
-}
-equalButton.addEventListener('click', equalOperation)
-
-
-//Print Numbers
-function addZero() {
-  resultSpace.value = num0.value
-}
-
-num0.addEventListener('click', addZero)
-
-function addOne() {
-  resultSpace.value = num1.value
-}
-
-num1.addEventListener('click', addOne)

cac682aeed6c1ed3728d5a64fd8db87c33cd9722
Filipe William <lipe.will@outlook.com>
2020-09-29 17:41:33 -0300

    Adicionada função que leva o último para o primeiro

--- Image Slider/script.js
+++ Image Slider/script.js
@@ -30,10 +30,15 @@ prevButton.addEventListener('click', prevImage);
 
 //Stop the movement
 function repeatMovement() {
-  console.log(slidesImg[i]);
+  console.log(i);
   if (slidesImg[i].id === 'yellow') {
     slidesDiv.style.transition = 'none';
     i = slidesImg.length - 2;
+    slidesDiv.style.transform = 'translateX(' + (-size * i) + "px)";
+  }
+  if (slidesImg[i].id === 'purple') {
+    slidesDiv.style.transition = 'none';
+    i = slidesImg.length - i;
     slidesDiv.style.transform = 'translateX(' + (-size * i) + "px)";
   }
 };
